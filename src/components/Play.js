864db731c7597a65ef687254e13df68933f613c8
Yuvraj518 <124797389+Yuvraj518@users.noreply.github.com>
2023-12-23 13:32:36 +0530

    submitting code-Sat Dec 23 2023 13:32:33 GMT+0530 (India Standard Time)

---
+++ src/components/Play.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Play=()=>{
+    
+    return (<div>
+        <p>
+            <h1>I am learning React. My life is getting better.</h1>
+        </p>
+    </div>)
+}
+
+export default Play;

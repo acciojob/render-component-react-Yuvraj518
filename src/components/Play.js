4e6bc90483282dfabd21bcfbcd98a7beb40cc090
Yuvraj518 <124797389+Yuvraj518@users.noreply.github.com>
2023-12-23 13:39:19 +0530

    submitting code-Sat Dec 23 2023 13:39:14 GMT+0530 (India Standard Time)

--- src/components/Play.js
+++ src/components/Play.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Play=()=>{
     
-    return (<div>
+    return (<div id="root">
         <p>
             <h1>I am learning React. My life is getting better.</h1>
         </p>
